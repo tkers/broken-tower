12e088eaf06b89a8bd71bdf677825bfcb08a0a02
mbonilla <margot.bonilla@reaktor.com>
2020-02-01 12:42:09 +0100

    player started swip

--- components/player-started.js
+++ components/player-started.js
@@ -1,26 +1,44 @@
 import React from "react";
 import Head from "next/head";
 import Nav from "./nav";
+import {Swipeable} from "react-swipeable";
 
-// socket.emit("send-piece", { piece: "piece" });
+const PlayerStarted = ({gameId, pieces, socket}) => {
 
-const PlayerStarted = ({ gameId, pieces, socket }) => (
-  <div>
-    <Head>
-      <title>Player</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    const swipedUp = (e, deltaY, isFlick) => {
+        const {pieces} = this.state
+        console.log("You Swiped Up...", e, deltaY, isFlick)
+        if (pieces.length) {
+            const biggerPiece = Math.max(...pieces)
+            this.socket.emit("send-piece", biggerPiece)
+            this.setState({pieces: pieces.filter(p => p !== biggerPiece)})
+        }
+    }
 
-    <Nav />
+    return <Swipeable
+        onSwipedUp={swipedUp}>
+        <div className={'player-board'}><Head>
+            <title>Player</title>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
 
-    <div>
-      Game started
-      <br />
-      Game ID: {gameId}
-      <br />
-      Pieces: {pieces.join(", ")}
-    </div>
-  </div>
-);
+            <Nav/>
+
+            <div>
+                Game started
+                <br/>
+                Game ID: {gameId}
+                <br/>
+                Pieces: {pieces.join(", ")}
+            </div>
+        </div>
+        <style jsx>{`
+                      .player-board {
+                        background-color: lightblue;
+                        height: 100vh;
+                      }
+                    `}</style>
+    </Swipeable>
+}
 
 export default PlayerStarted;
--- pages/player.js
+++ pages/player.js
@@ -1,7 +1,5 @@
 import React from "react";
 import io from "socket.io-client";
-import {Swipeable} from 'react-swipeable'
-
 import PlayerWaiting from "../components/player-waiting";
 import PlayerStarted from "../components/player-started";
 
@@ -9,28 +7,6 @@ class PlayerPage extends React.Component {
     // connect to WS server and listen event
 
     state = {gameId: null, started: false, playerCount: 0, pieces: []};
-
-    swiping(e, deltaX, deltaY, absX, absY, velocity) {
-        console.log("You're Swiping...", e, deltaX, deltaY, absX, absY, velocity)
-    }
-
-    swipingLeft(e, absX) {
-        console.log("You're Swiping to the Left...", e, absX)
-    }
-
-    swiped(e, deltaX, deltaY, isFlick, velocity) {
-        console.log("You Swiped...", e, deltaX, deltaY, isFlick, velocity)
-    }
-
-    swipedUp = (e, deltaY, isFlick) => {
-        const { pieces } = this.state
-        console.log("You Swiped Up...", e, deltaY, isFlick)
-        if (pieces.length) {
-            const biggerPiece = Math.max(...pieces)
-            this.socket.emit("send-piece", biggerPiece)
-            this.setState({pieces: pieces.filter(p => p !== biggerPiece)})
-        }
-    }
 
     componentDidMount() {
         this.socket = io();
@@ -77,36 +53,18 @@ class PlayerPage extends React.Component {
     };
 
     render() {
-        return (
-                <Swipeable
-                    onSwiping={this.swiping}
-                    onSwipingLeft={this.swipingLeft}
-                    onSwiped={this.swiped}
-                    onSwipedUp={this.swipedUp}>
-                    <div className={'player-board'}>
-
-                    {this.state.started ? (
-                        <PlayerStarted
-                            gameId={this.state.gameId}
-                            pieces={this.state.pieces}
-                            socket={this.socket}
-                        />
-                    ) : (
-                        <PlayerWaiting
-                            gameId={this.state.gameId}
-                            playerCount={this.state.playerCount}
-                            socket={this.socket}
-                        />)
-                    }
-                    </div>
-                    <style jsx>{`
-                      .player-board {
-                        background-color: lightblue;
-                        height: 100vh;
-                      }
-                    `}</style>
-                </Swipeable>
-        )
+        return this.state.started ? (
+            <PlayerStarted
+                gameId={this.state.gameId}
+                pieces={this.state.pieces}
+                socket={this.socket}
+            />
+        ) : (
+            <PlayerWaiting
+                gameId={this.state.gameId}
+                playerCount={this.state.playerCount}
+                socket={this.socket}
+            />)
     }
 }
 
