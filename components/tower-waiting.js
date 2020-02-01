65798120b864224b44b8c5b9e7f87b2387b9506f
Tijn Kersjes <tijn@divbyzero.nl>
2020-02-01 13:02:37 +0100

    Add join url to tower waiting

--- components/tower-waiting.js
+++ components/tower-waiting.js
@@ -10,83 +10,88 @@ const TowerWaiting = ({
   ip,
   port,
   countdown
-}) => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+}) => {
+  const url = `http://${ip}:${port}/player?gameId=${gameId}`;
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <Nav />
+      <Nav />
 
-    <div className="hero">
-      <h1 className="title">{gameId}</h1>
-      <p className="description">
-        <QRCode
-          value={`http://${ip}:${port}/player?gameId=${gameId}`}
-          size={512}
-          bgColor="#FFFF00"
-          fgColor="#000000"
-          includeMargin={true}
-        />
-        {countdown && <h1 className="title">Starting in {countdown}...</h1>}
-      </p>
+      <div className="hero">
+        <h1 className="title">
+          <a href={url}>{gameId}</a>
+        </h1>
+        <p className="description">
+          <QRCode
+            value={url}
+            size={512}
+            bgColor="#FFFF00"
+            fgColor="#000000"
+            includeMargin={true}
+          />
+          {countdown && <h1 className="title">Starting in {countdown}...</h1>}
+        </p>
 
-      <div className="row">
-        <a href="#" onClick={onStart} className="card">
-          <h3>Start Game!</h3>
-          <p>{playerCount} players waiting</p>
-        </a>
+        <div className="row">
+          <a href="#" onClick={onStart} className="card">
+            <h3>Start Game!</h3>
+            <p>{playerCount} players waiting</p>
+          </a>
+        </div>
       </div>
-    </div>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-);
+      <style jsx>{`
+        .hero {
+          width: 100%;
+          color: #333;
+        }
+        .title {
+          margin: 0;
+          width: 100%;
+          padding-top: 80px;
+          line-height: 1.15;
+          font-size: 48px;
+        }
+        .title,
+        .description {
+          text-align: center;
+        }
+        .row {
+          max-width: 880px;
+          margin: 80px auto 40px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-around;
+        }
+        .card {
+          padding: 18px 18px 24px;
+          width: 220px;
+          text-align: left;
+          text-decoration: none;
+          color: #434343;
+          border: 1px solid #9b9b9b;
+        }
+        .card:hover {
+          border-color: #067df7;
+        }
+        .card h3 {
+          margin: 0;
+          color: #067df7;
+          font-size: 18px;
+        }
+        .card p {
+          margin: 0;
+          padding: 12px 0 0;
+          font-size: 13px;
+          color: #333;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default TowerWaiting;
