4d7e4735cce98309b0aa82fb4a51a1ed14dfab59
Tijn Kersjes <tijn@divbyzero.nl>
2020-02-01 15:56:51 +0100

    Refactor index to use hooks

--- pages/index.js
+++ pages/index.js
@@ -1,128 +1,89 @@
-import React from "react";
+import React, { useEffect, useState, useReducer, useRef } from "react";
 import io from "socket.io-client";
 
 import TowerWaiting from "../components/tower-waiting";
 import TowerStarted from "../components/tower-started";
 import TowerDisconnected from "../components/tower-disconnected";
 
-class Home extends React.Component {
-  // connect to WS server and listen event
-  state = { connected: false, started: false, matchId: null, pieces: [] };
+const Home = () => {
+  const [match, setMatch] = useState({ started: false, id: null });
+  const [address, setAddress] = useState({ ip: null, port: null });
 
-  componentDidMount() {
-    this.socket = io();
+  const socket = useRef();
+  const [connected, setConnected] = useState(false);
+  const [playerCount, addPlayerCount] = useReducer(
+    (total, delta) => total + delta,
+    0
+  );
+  const [pieces, addPiece] = useReducer(
+    (pieces, newPiece) => [...pieces, newPiece],
+    []
+  );
 
-    this.socket.on("connect", () => {
-      this.setState({ connected: true });
-      this.socket.emit("create-match", data => {
-        console.log("createmathc reply:", data);
-        const matchId = data.matchId;
-        this.setState({
-          matchId,
-          playerCount: 0,
-          ip: data.ip,
-          port: data.port
-        });
+  useEffect(() => {
+    const sock = io();
+
+    sock.on("connect", () => {
+      setConnected(true);
+      sock.emit("create-match", data => {
+        setMatch({ id: data.matchId, started: false });
+        setAddress({ ip: data.ip, port: data.port });
       });
     });
 
-    this.socket.on("disconnect", () => {
-      this.setState({ connected: false });
+    sock.on("disconnect", () => {
+      setConnected(false);
     });
 
-    this.socket.on("player-join", () => {
-      this.setState({ playerCount: this.state.playerCount + 1 }, () => {
-        if (this.state.playerCount >= 2) {
-          this.restartCountdown();
-        }
-      });
+    sock.on("player-join", () => {
+      addPlayerCount(1);
     });
 
-    this.socket.on("player-leave", () => {
-      this.setState({ playerCount: this.state.playerCount - 1 }, () => {
-        if (this.state.playerCount < 2) {
-          this.cancelCountdown();
-        }
-      });
+    sock.on("player-leave", () => {
+      addPlayerCount(-1);
     });
 
-    this.socket.on("send-piece", data => {
-      this.receivePiece(data);
+    sock.on("send-piece", size => {
+      addPiece(size);
     });
-  }
 
-  // close socket connection
-  componentWillUnmount() {
-    this.socket.off("message", this.handleMessage);
-    this.socket.close();
-  }
+    socket.current = sock;
 
-  restartCountdown() {
-    clearInterval(this.countInterval);
-    this.setState({ countdown: 30 });
-    this.countInterval = setInterval(this.tick, 1000);
-  }
+    return () => {
+      socket.current && socket.current.close();
+    };
+  }, []);
 
-  cancelCountdown() {
-    clearInterval(this.countInterval);
-    this.setState({ countdown: null });
-  }
-
-  tick = () => {
-    if (this.state.countdown === 0) {
-      this.cancelCountdown();
-      this.startGame();
-    } else {
-      this.setState({ countdown: this.state.countdown - 1 });
-    }
-  };
-
-  startGame = () => {
-    this.socket.emit("start-match", data => {
+  const startMatch = () => {
+    socket.current.emit("start-match", data => {
       if (data.error) {
         alert(data.error);
       } else {
-        console.log("Game started!", data);
-        this.setState({ started: true });
+        console.log("Match started!", data);
+        setMatch({ ...match, started: true });
       }
     });
   };
 
-  receivePiece = piece => {
-    console.log("received", piece);
-    this.setState(state => {
-      console.log({ state });
-      return {
-        pieces: [...state.pieces, piece]
-      };
-    });
-  };
-
-  render() {
-    console.log(this.state);
-    return this.state.connected ? (
-      this.state.started ? (
-        <TowerStarted
-          matchId={this.state.matchId}
-          playerCount={this.state.playerCount}
-          socket={this.socket}
-          pieces={this.state.pieces}
-        />
-      ) : (
-        <TowerWaiting
-          matchId={this.state.matchId}
-          ip={this.state.ip}
-          port={this.state.port}
-          playerCount={this.state.playerCount}
-          onStart={this.startGame}
-          countdown={this.state.countdown}
-          socket={this.socket}
-        />
-      )
+  return connected ? (
+    match.started ? (
+      <TowerStarted
+        matchId={match.id}
+        playerCount={playerCount}
+        pieces={pieces}
+      />
     ) : (
-      <TowerDisconnected />
-    );
-  }
-}
+      <TowerWaiting
+        matchId={match.id}
+        playerCount={playerCount}
+        onStart={startMatch}
+        ip={address.ip}
+        port={address.port}
+      />
+    )
+  ) : (
+    <TowerDisconnected />
+  );
+};
 
 export default Home;
