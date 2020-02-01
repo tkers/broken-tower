8c8647a985c3f585c21859c189d0b379bd692fc2
David Vázquez Púa <davazp@gmail.com>
2020-02-01 09:28:54 +0100

    Animate the tower

--- pages/tower.js
+++ pages/tower.js
@@ -1,9 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Canvas, useFrame, useThree } from "react-three-fiber";
 
 const range = i => new Array(i + 1).fill(0).map((_, i) => i);
 
-const getPieces = n => range(n - 1).map(i => i + 1);
+const getPieces = n =>
+  range(n - 1)
+    .map(i => i + 1)
+    .reverse();
 
 function Piece(props) {
   // This reference will give us direct access to the mesh
@@ -27,15 +30,19 @@ function Piece(props) {
   );
 }
 
+const STACK_SIZE = 20;
+
 function Lookup() {
   const { camera } = useThree();
 
   let [angle, setAngle] = useState(0);
 
   useFrame(() => {
-    camera.position.x = 50 * Math.cos(angle);
-    camera.position.y = 0;
-    camera.position.z = 50 * Math.sin(angle);
+    let distance = 80;
+
+    camera.position.x = distance * Math.cos(angle);
+    camera.position.y = 2 * STACK_SIZE;
+    camera.position.z = distance * Math.sin(angle);
 
     camera.lookAt(0, 0, 0);
     setAngle(angle + 0.005);
@@ -45,21 +52,30 @@ function Lookup() {
 }
 
 function Tower() {
+  let [pieces, setPieces] = useState([20]);
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      let last = pieces[pieces.length - 1];
+      if (last > 1) {
+        setPieces([...pieces, last - 1]);
+      } else {
+        setPieces([20]);
+      }
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [pieces]);
+
   return (
-    <Canvas
-      style={{ height: 800, width: 800 }}
-      camera={{ position: [10, 0, 50] }}
-    >
+    <Canvas style={{ height: 800, width: 800 }}>
       <Lookup />
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      {getPieces(20).map(i => (
-        <Piece key={i} position={[0, -i, 0]} width={i} />
+      <pointLight position={[10, STACK_SIZE, 10]} />
+      {pieces.map((width, i) => (
+        <Piece key={width} position={[0, i, 0]} width={width} />
       ))}
     </Canvas>
   );
-
-  // return <Stack pieces={getPieces(60)} />;
 }
 
 export default Tower;
