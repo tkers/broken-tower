8d59b524c663992659dafb98e837000699d0bec0
David Vázquez Púa <davazp@gmail.com>
2020-02-01 13:22:22 +0100

    Format code

--- components/player-started.js
+++ components/player-started.js
@@ -1,34 +1,35 @@
 import React from "react";
 import Head from "next/head";
 import Nav from "./nav";
-import {Swipeable} from "react-swipeable";
+import { Swipeable } from "react-swipeable";
 
-const PlayerStarted = ({gameId, pieces, sendPiece}) => {
-
-    return <Swipeable
-        onSwipedUp={sendPiece}>
-        <div className={'player-board'}><Head>
-            <title>Player</title>
-            <link rel="icon" href="/favicon.ico"/>
+const PlayerStarted = ({ gameId, pieces, sendPiece }) => {
+  return (
+    <Swipeable onSwipedUp={sendPiece}>
+      <div className={"player-board"}>
+        <Head>
+          <title>Player</title>
+          <link rel="icon" href="/favicon.ico" />
         </Head>
 
-            <Nav/>
+        <Nav />
 
-            <div>
-                Game started
-                <br/>
-                Game ID: {gameId}
-                <br/>
-                Pieces: {pieces.join(", ")}
-            </div>
+        <div>
+          Game started
+          <br />
+          Game ID: {gameId}
+          <br />
+          Pieces: {pieces.join(", ")}
         </div>
-        <style jsx>{`
-                      .player-board {
-                        background-color: lightblue;
-                        height: 100vh;
-                      }
-                    `}</style>
+      </div>
+      <style jsx>{`
+        .player-board {
+          background-color: lightblue;
+          height: 100vh;
+        }
+      `}</style>
     </Swipeable>
-}
+  );
+};
 
 export default PlayerStarted;
