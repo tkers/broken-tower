652626fe3e1ee9f9dcb2945b3f9d27861ea71c67
Tijn Kersjes <tijn@divbyzero.nl>
2020-02-02 14:31:52 +0100

    Fix NaN warning

--- components/rating.js
+++ components/rating.js
@@ -2,8 +2,9 @@ export const countWrongPieces = pieces => {
   return pieces.filter(p => !p.stable).length;
 };
 
-export const getScore = (pieces, remainingPieces = 0) => {
+export const getScore = (pieces = [], remainingPieces = 0) => {
   const total = pieces.length + remainingPieces;
+  if (total === 0) return 0;
   const good = pieces.length - countWrongPieces(pieces);
   return Math.floor((good * 1000) / total);
 };
